778cfa2a5ac2fd1735dd159bd6e284854b982001
NolanDu <76052692+NolanDu@users.noreply.github.com>
2022-06-15 13:51:30 -0400

    Add files via upload

--- test2/src/Tester.java
+++ test2/src/Tester.java
@@ -65,25 +65,25 @@ public static void main(String[] args) throws Exception {
         if(totalMasterCBM>88){
             HQAndGP = Math.floor(totalMasterCBM/63)*17125+Math.ceil((totalMasterCBM-(Math.floor(totalMasterCBM/63)*63))/57)*16875;
         }
-        //under thus, values too large and equalling 2 when .floor
+        //under this, values have higher chance of error 
 
         if(totalMasterCBM>120&&totalMasterCBM<140.444){
-            //going negative
-            HQAndGPAndLCL = Math.floor(totalMasterCBM/63)*17125+16875+(totalMasterCBM-(Math.floor(totalMasterCBM/63)*63+57))*675;
+            //never optimal
+            HQAndGPAndLCL = 0;
+            //Math.floor(totalMasterCBM/63)*17125+16875+(totalMasterCBM-(Math.floor(totalMasterCBM/63)*63+57))*675;
         }    
         
-        if(totalMasterCBM>108.444){
-            //value too high because Math.floor comes out to 2
-            HQAndGPAnd20 = Math.floor(totalMasterCBM/63)*17125+16875+13800;
+        if(totalMasterCBM>108.444&&(totalMasterCBM-57-27)%63==0){
+            //this is only the optimal value when cbm is equal to 147 or a multiple
+            HQAndGPAnd20 = ((totalMasterCBM-57-27)/63)*17125+16875+13800;
+            //Math.floor(totalMasterCBM/63)*17125+16875+13800
         }
+        
         if(totalMasterCBM>147&&totalMasterCBM<210){    
-        //going negative
-        HQAndGPAnd20AndLCL = Math.floor(totalMasterCBM/63)*17125+16875+13800+(totalMasterCBM-(Math.floor(totalMasterCBM/63)*63+57+27))*675;
-        }
-
-        if(totalMasterCBM>210){
-        HQAndGPAnd20AndLCL = Math.floor(totalMasterCBM/63)*17125+16875+13800+(totalMasterCBM-(Math.floor(totalMasterCBM/63)*63+57+27))*675; 
-        }    
+            //never optimal
+            HQAndGPAnd20AndLCL = 0;
+            //Math.floor((totalMasterCBM-57-27)/63)*17125+16875+13800+(totalMasterCBM-(Math.floor((totalMasterCBM-57-27)/63)*63))*675;
+        }  
     
         System.out.println("LCL: "+LCL);
         System.out.println("ctn20: "+ctn20);
@@ -117,70 +117,83 @@ public static void main(String[] args) throws Exception {
 	            }
 	        }
         
-        switch(indexOfCheapest){
-            case 0:
-                System.out.println("The most optimal freight solution is to ship LCL for a cost of $"+LCL);
-                System.out.println("Cost/piece: $"+LCL/totalPieces);
-                break;
-            case 1:
-                System.out.println("The most optimal freight solution is to ship "+Math.ceil(totalMasterCBM/27)+" 20' containers for a cost of $"+ctn20);
-                System.out.println("Cost/piece: $"+ctn20/totalPieces);
-                break;
-            case 2:
-                System.out.println("The most optimal freight solution is to ship "+Math.floor(totalMasterCBM/27)+" 20' containers and the rest with LCL for a cost of $"+ctn20AndLCL);
-                System.out.println("Cost/piece: $"+ctn20AndLCL/totalPieces);
-                break;
-            case 3:
-                System.out.println("The most optimal freight solution is to ship "+Math.ceil(totalMasterCBM/57)+" 40'GP containers for a cost of $"+GP);
-                System.out.println("Cost/piece: $"+GP/totalPieces);
-                break;
-            case 4:
-                System.out.println("The most optimal freight solution is to ship "+Math.floor(totalMasterCBM/57)+" 40'GP containers and the rest with LCL for a cost of $"+GPAndLCL);
-                System.out.println("Cost/piece: $"+GPAndLCL/totalPieces);
-                break;
-            case 5:
-                System.out.println("The most optimal freight solution is to ship "+Math.floor(totalMasterCBM/57)+" 40'GP containers and the rest with one 20' for a cost of $"+GPAnd20);
-                System.out.println("Cost/piece: $"+GPAnd20/totalPieces);
-                break;
-            case 6:
-                System.out.println("The most optimal freight solution is to ship "+Math.floor(totalMasterCBM/57)+" 40'GP containers and the rest with one 20' and LCL for a cost of $"+GPAnd20AndLCL);
-                System.out.println("Cost/piece: $"+GPAnd20AndLCL/totalPieces);
-                break;
-            case 7:
-                System.out.println("The most optimal freight solution is to ship "+Math.ceil(totalMasterCBM/63)+" 40'HQ containers for a cost of $"+HQ);
-                System.out.println("Cost/piece: $"+HQ/totalPieces);
-                break;
-            case 8:
-                System.out.println("The most optimal freight solution is to ship "+Math.floor(totalMasterCBM/63)+" 40'HQ containers and the rest with LCL for a cost of $"+HQAndLCL);
-                System.out.println("Cost/piece: $"+HQAndLCL/totalPieces);
-                break;
-            case 9:
-                System.out.println("The most optimal freight solution is to ship "+Math.floor(totalMasterCBM/63)+" 40'HQ containers and the rest with one 20' container for a cost of $"+HQAnd20);
-                System.out.println("Cost/piece: $"+HQAnd20/totalPieces);
-                break;
-            case 10:
-                System.out.println("The most optimal freight solution is to ship "+Math.floor(totalMasterCBM/63)+" 40'HQ containers and the rest with one 20' and LCL for a cost of $"+HQAnd20AndLCL);
-                System.out.println("Cost/piece: $"+HQAnd20AndLCL/totalPieces);
-                break;
-            case 11:
-                System.out.println("The most optimal freight solution is to ship "+Math.floor(totalMasterCBM/63)+" 40'HQ containers and the rest with one 40'GP container for a cost of $"+HQAndGP);
-                System.out.println("Cost/piece: $"+HQAndGP/totalPieces);
-                break;
-            case 12:
-                System.out.println("The most optimal freight solution is to ship "+Math.floor(totalMasterCBM/63)+" 40'HQ containers and the rest with one 40'GP and LCL for a cost of $"+HQAndGPAndLCL);
-                System.out.println("Cost/piece: $"+HQAndGPAndLCL/totalPieces);
-                break;
-            case 13:
-                System.out.println("The most optimal freight solution is to ship "+Math.floor(totalMasterCBM/63)+" 40'HQ containers and the rest with one 40'GP and one 20' for a cost of $"+HQAndGPAnd20);
-                System.out.println("Cost/piece: $"+HQAndGPAnd20/totalPieces);
-                break;
-            case 14:
-                System.out.println("The most optimal freight solution is to ship "+Math.floor(totalMasterCBM/63)+" 40'HQ containers and the rest with one 40'GP and one 20' and LCL for a cost of $"+HQAndGPAnd20AndLCL);
-                System.out.println("Cost/piece: $"+HQAndGPAnd20AndLCL/totalPieces);
-                break;
-            }
-        System.out.println((totalMasterCBM%(57+27))%57); 
-        
+            switch(indexOfCheapest){
+                case 0:
+                    System.out.println("The most optimal freight solution is to ship LCL for a cost of $"+LCL);
+                    double c0 = (double)Math.round(LCL/totalPieces * 1000)/1000;
+                    System.out.println("Cost/piece: $"+c0);
+                    break;
+                case 1:
+                    System.out.println("The most optimal freight solution is to ship "+Math.ceil(totalMasterCBM/27)+" 20' container(s) for a cost of $"+ctn20);
+                    double c1 = (double)Math.round(ctn20/totalPieces * 1000)/1000;
+                    System.out.println("Cost/piece: $"+c1);
+                    break;
+                case 2:
+                    System.out.println("The most optimal freight solution is to ship "+Math.floor(totalMasterCBM/27)+" 20' container(s) and the rest with LCL for a cost of $"+ctn20AndLCL);
+                    double c2 = (double)Math.round(ctn20AndLCL/totalPieces * 1000)/1000;
+                    System.out.println("Cost/piece: $"+c2);
+                    break;
+                case 3:
+                    System.out.println("The most optimal freight solution is to ship "+Math.ceil(totalMasterCBM/57)+" 40'GP container(s) for a cost of $"+GP);
+                    double c3 = (double)Math.round(GP/totalPieces * 1000)/1000;
+                    System.out.println("Cost/piece: $"+c3);
+                    break;
+                case 4:
+                    System.out.println("The most optimal freight solution is to ship "+Math.floor(totalMasterCBM/57)+" 40'GP container(s) and the rest with LCL for a cost of $"+GPAndLCL);
+                    double c4 = (double)Math.round(GPAndLCL/totalPieces * 1000)/1000;
+                    System.out.println("Cost/piece: $"+c4);
+                    break;
+                case 5:
+                    System.out.println("The most optimal freight solution is to ship "+Math.floor(totalMasterCBM/57)+" 40'GP container(s) and the rest with one 20' for a cost of $"+GPAnd20);
+                    double c5 = (double)Math.round(GPAnd20/totalPieces * 1000)/1000;
+                    System.out.println("Cost/piece: $"+c5);
+                    break;
+                case 6:
+                    System.out.println("The most optimal freight solution is to ship "+Math.floor(totalMasterCBM/57)+" 40'GP container(s) and the rest with one 20' and LCL for a cost of $"+GPAnd20AndLCL);
+                    double c6 = (double)Math.round(GPAnd20AndLCL/totalPieces * 1000)/1000;
+                    System.out.println("Cost/piece: $"+c6);
+                    break;
+                case 7:
+                    System.out.println("The most optimal freight solution is to ship "+Math.ceil(totalMasterCBM/63)+" 40'HQ container(s) for a cost of $"+HQ);
+                    double c7 = (double)Math.round(HQ/totalPieces * 1000)/1000;
+                    System.out.println("Cost/piece: $"+c7);
+                    break;
+                case 8:
+                    System.out.println("The most optimal freight solution is to ship "+Math.floor(totalMasterCBM/63)+" 40'HQ container(s) and the rest with LCL for a cost of $"+HQAndLCL);
+                    double c8 = (double)Math.round(HQAndLCL/totalPieces * 1000)/1000;
+                    System.out.println("Cost/piece: $"+c8);
+                    break;
+                case 9:
+                    System.out.println("The most optimal freight solution is to ship "+Math.floor(totalMasterCBM/63)+" 40'HQ container(s) and the rest with one 20' container for a cost of $"+HQAnd20);
+                    double c9 = (double)Math.round(HQAnd20/totalPieces * 1000)/1000;
+                    System.out.println("Cost/piece: $"+c9);
+                    break;
+                case 10:
+                    System.out.println("The most optimal freight solution is to ship "+Math.floor(totalMasterCBM/63)+" 40'HQ container(s) and the rest with one 20' and LCL for a cost of $"+HQAnd20AndLCL);
+                    double c10 = (double)Math.round(HQAnd20AndLCL/totalPieces * 1000)/1000;
+                    System.out.println("Cost/piece: $"+c10);
+                    break;
+                case 11:
+                    System.out.println("The most optimal freight solution is to ship "+Math.floor(totalMasterCBM/63)+" 40'HQ container(s) and the rest with one 40'GP container for a cost of $"+HQAndGP);
+                    double c11 = (double)Math.round(HQAndGP/totalPieces * 1000)/1000;
+                    System.out.println("Cost/piece: $"+c11);
+                    break;
+                case 12:
+                    System.out.println("The most optimal freight solution is to ship "+Math.floor(totalMasterCBM/63)+" 40'HQ container(s) and the rest with one 40'GP and LCL for a cost of $"+HQAndGPAndLCL);
+                    double c12 = (double)Math.round(HQAndGPAndLCL/totalPieces * 1000)/1000;
+                    System.out.println("Cost/piece: $"+c12);
+                    break;
+                case 13:
+                    System.out.println("The most optimal freight solution is to ship "+(totalMasterCBM-57-27)/63+" 40'HQ container(s) and the rest with one 40'GP and one 20' for a cost of $"+HQAndGPAnd20);
+                    double c13 = (double)Math.round(HQAndGPAnd20/totalPieces * 1000)/1000;
+                    System.out.println("Cost/piece: $"+c13);
+                    break;
+                case 14:
+                    System.out.println("The most optimal freight solution is to ship "+Math.floor((totalMasterCBM-57-27)/63)+" 40'HQ container(s) and the rest with one 40'GP and one 20' and LCL for a cost of $"+HQAndGPAnd20AndLCL);
+                    double c14 = (double)Math.round(HQAndGPAnd20AndLCL/totalPieces * 1000)/1000;
+                    System.out.println("Cost/piece: $"+c14);
+                    break;
+                }
     }
        
           
